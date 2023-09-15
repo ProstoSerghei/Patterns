8023f5e944235d7f6ca78ad0061388d2abf36537
Serghei <prosto.serghei@yandex.ru>
2023-09-15 17:47:50 +0300

    Done

---
+++ lesson_5_6/simba_framework/main.py
@@ -0,0 +1,79 @@
+from quopri import decodestring
+from .requests_config import GetRequests, PostRequests
+
+
+class PageNotFound404:
+    def __call__(self, request):
+        return '404 WHAT', '404 PAGE Not Found'
+
+
+class Framework:
+
+    """Класс Framework - основа фреймворка"""
+
+    def __init__(self, routes_obj, fronts_obj):
+        self.routes_lst = routes_obj
+        self.fronts_lst = fronts_obj
+
+    def __call__(self, environ, start_response):
+        path = environ['PATH_INFO']
+
+        if not path.endswith('/'):
+            path = f'{path}/'
+
+        request = {}
+        method = environ['REQUEST_METHOD']
+        request['method'] = method
+
+        if method == 'POST':
+            data = PostRequests().get_request_params(environ)
+            request['data'] = Framework.decode_value(data)
+            print(f'Нам пришёл post-запрос: {Framework.decode_value(data)}')
+        if method == 'GET':
+            request_params = GetRequests().get_request_params(environ)
+            request['request_params'] = Framework.decode_value(request_params)
+            print(f'Нам пришли GET-параметры:'
+                  f' {Framework.decode_value(request_params)}')
+
+        if path in self.routes_lst:
+            view = self.routes_lst[path]
+        else:
+            view = PageNotFound404()
+
+        for front in self.fronts_lst:
+            front(request)
+        code, body = view(request)
+        start_response(code, [('Content-Type', 'text/html')])
+        return [body.encode('utf-8')]
+
+    @staticmethod
+    def decode_value(data):
+        new_data = {}
+        for k, v in data.items():
+            val = bytes(v.replace('%', '=').replace("+", " "), 'UTF-8')
+            val_decode_str = decodestring(val).decode('UTF-8')
+            new_data[k] = val_decode_str
+        return new_data
+
+
+class DebugApplication(Framework):
+
+    def __init__(self, routes_obj, fronts_obj):
+        self.application = Framework(routes_obj, fronts_obj)
+        super().__init__(routes_obj, fronts_obj)
+
+    def __call__(self, env, start_response):
+        print('DEBUG MODE')
+        print(env)
+        return self.application(env, start_response)
+
+
+class FakeApplication(Framework):
+
+    def __init__(self, routes_obj, fronts_obj):
+        self.application = Framework(routes_obj, fronts_obj)
+        super().__init__(routes_obj, fronts_obj)
+
+    def __call__(self, env, start_response):
+        start_response('200 OK', [('Content-Type', 'text/html')])
+        return [b'Hello from Fake']
